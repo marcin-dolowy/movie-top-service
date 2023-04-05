666373638714d1d15e763137d937f3bb9a8a58ff
Marcin Dołowy <marcindolowy00@gmail.com>
2023-04-05 23:56:55 +0200

    Create table without poster for movies loaded from api

--- src/main/java/pl/dolowy/movietopservice/MyController.java
+++ src/main/java/pl/dolowy/movietopservice/MyController.java
@@ -1,25 +1,81 @@
 package pl.dolowy.movietopservice;
 
-import javafx.event.ActionEvent;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.image.Image;
+import javafx.scene.text.Text;
+import lombok.RequiredArgsConstructor;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Controller;
+import pl.dolowy.movietopservice.model.Movie;
+import pl.dolowy.movietopservice.service.MovieService;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
+@RequiredArgsConstructor
 @FxmlView("main-stage.fxml")
 public class MyController {
 
-    @FXML
-    public CheckBox checkBox;
-    @FXML
-    private Label nameLabel;
+    private final MovieService movieService;
 
     @FXML
-    public void showMyName() {
-        nameLabel.setText("Witaj, Marcin Dołowy");
+    private TextField movieSearchTextField;
+
+    @FXML
+    private Button searchButton;
+
+    @FXML
+    private TableView<Movie> moviesTableView = new TableView<>();
+
+    @FXML
+    private TableColumn<Movie, String> imdbIDTableColumn;
+    @FXML
+    private TableColumn<Movie, String> titleTableColumn;
+    @FXML
+    private TableColumn<Movie, String> typeTableColumn;
+    @FXML
+    private TableColumn<Movie, LocalDate> releasedTableColumn;
+    @FXML
+    private TableColumn<Movie, String> directorTableColumn;
+    @FXML
+    private TableColumn<Movie, String> plotTableColumn;
+
+
+    @FXML
+    void findMovies() {
+        List<Movie> movies = movieService.getMoviesFromApi(movieSearchTextField.getText());
+
+        List<Image> imagesFromMovies = movies.stream().map(Movie::getPoster)
+                .map(Image::new)
+                .collect(Collectors.toList());
+
+        ObservableList<Movie> data = FXCollections.observableList(movies);
+
+        imdbIDTableColumn.setCellValueFactory((new PropertyValueFactory<>("imdbID")));
+        titleTableColumn.setCellValueFactory((new PropertyValueFactory<>("title")));
+        typeTableColumn.setCellValueFactory((new PropertyValueFactory<>("type")));
+        releasedTableColumn.setCellValueFactory((new PropertyValueFactory<>("released")));
+        directorTableColumn.setCellValueFactory((new PropertyValueFactory<>("director")));
+        plotTableColumn.setCellValueFactory((new PropertyValueFactory<>("plot")));
+
+        // Cell Factory to wrap text for plot
+        plotTableColumn.setCellFactory(tc -> {
+            TableCell<Movie, String> cell = new TableCell<>();
+            Text text = new Text();
+            cell.setGraphic(text);
+            cell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+            text.wrappingWidthProperty().bind(plotTableColumn.widthProperty());
+            text.textProperty().bind(cell.itemProperty());
+            return cell;
+        });
+
+        moviesTableView.setItems(data);
     }
-
 
 }
