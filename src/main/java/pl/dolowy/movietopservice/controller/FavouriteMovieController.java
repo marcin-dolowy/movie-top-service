ceec62db26c88378ee0353843cbd3a606919f0a9
Marcin Dołowy <marcindolowy00@gmail.com>
2023-04-08 01:10:34 +0200

    Add controller for favourite movies

---
+++ src/main/java/pl/dolowy/movietopservice/controller/FavouriteMovieController.java
@@ -0,0 +1,9 @@
+package pl.dolowy.movietopservice.controller;
+
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@FxmlView("favourite-movie.fxml")
+public class FavouriteMovieController {
+}
