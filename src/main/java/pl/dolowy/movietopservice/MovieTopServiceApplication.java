22df5980b2ee66a67e6ec3c86322bb21d5b654b0
Marcin Dołowy <marcindolowy00@gmail.com>
2023-04-09 17:53:55 +0200

    Clean MovieTopServiceApplication

--- src/main/java/pl/dolowy/movietopservice/MovieTopServiceApplication.java
+++ src/main/java/pl/dolowy/movietopservice/MovieTopServiceApplication.java
@@ -8,17 +8,11 @@ import net.rgielen.fxweaver.spring.InjectionPointLazyFxControllerAndViewResolver
 import net.rgielen.fxweaver.spring.SpringFxWeaver;
 import org.springframework.beans.factory.InjectionPoint;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Scope;
 import pl.dolowy.movietopservice.application.JavaFxApplication;
-import pl.dolowy.movietopservice.model.Movie;
-import pl.dolowy.movietopservice.service.MovieService;
-
-import java.util.List;
 
 @SpringBootApplication
 public class MovieTopServiceApplication {
@@ -39,13 +33,4 @@ public class MovieTopServiceApplication {
         return new InjectionPointLazyFxControllerAndViewResolver(fxWeaver)
                 .resolve(injectionPoint);
     }
-//    @Bean
-//    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-//        return args -> {
-//            MovieService movieService = new MovieService();
-//            List<Movie> movies = movieService.getMoviesFromApi("Harry Potter");
-//            System.out.println(movies.size());
-//            movies.forEach(System.out::println);
-//        };
-//    }
 }
